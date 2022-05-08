a77fd9b2f2b56a27072d2dc7699911ee3bfd2654
andrew <andrewgable@gmail.com>
2022-05-07 23:57:16 -0600

    Use SSR version

--- gatsby-theme-serafina/src/hooks/use-window-dimensions.js
+++ gatsby-theme-serafina/src/hooks/use-window-dimensions.js
@@ -1,24 +1,29 @@
 import {useState, useEffect} from 'react';
 
-function getWindowDimensions() {
-    const {innerWidth: width, innerHeight: height} = window;
-    return {
-        width,
-        height
-    };
-}
-
 export default function useWindowDimensions() {
+    const hasWindow = typeof window !== 'undefined';
+
+    function getWindowDimensions() {
+        const width = hasWindow ? window.innerWidth : null;
+        const height = hasWindow ? window.innerHeight : null;
+        return {
+            width,
+            height,
+        };
+    }
+
     const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
 
     useEffect(() => {
-        function handleResize() {
-            setWindowDimensions(getWindowDimensions());
-        }
+        if (hasWindow) {
+            function handleResize() {
+                setWindowDimensions(getWindowDimensions());
+            }
 
-        window.addEventListener('resize', handleResize);
-        return () => window.removeEventListener('resize', handleResize);
-    }, []);
+            window.addEventListener('resize', handleResize);
+            return () => window.removeEventListener('resize', handleResize);
+        }
+    }, [hasWindow]);
 
     return windowDimensions;
 }
