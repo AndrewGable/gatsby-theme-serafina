6009497ebf12c5de814bf82cb11137df5bd387be
Andrew Gable <andrew@expensify.com>
2022-08-05 22:51:48 -0600

    Undo access key and enable disable contact page

--- gatsby-theme-serafina/utils/default-options.js
+++ gatsby-theme-serafina/utils/default-options.js
@@ -2,7 +2,7 @@ module.exports = themeOptions => {
   const galleriesPath = themeOptions.contentPath || `src/galleries`;
   const pagesPath = themeOptions.contentPath || `src/pages`;
   const assetPath = themeOptions.assetPath || `src/assets`;
-  const disableContact = false;
+  const disableContact = themeOptions.disableContact || false;
   const spacing = themeOptions.spacing || 6;
   const layout = themeOptions.layout || "columns";
 
